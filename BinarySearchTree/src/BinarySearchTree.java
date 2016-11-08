4376eff973a6a7c2c6fdbe6531b8fd4ce8482c44
Shelby Huston <Shelby Huston@SatansEyepiece>
2016-11-07 23:21:03 -0700

    i finished delete

--- BinarySearchTree/src/BinarySearchTree.java
+++ BinarySearchTree/src/BinarySearchTree.java
@@ -1,115 +1,177 @@
 import java.util.Scanner;
 
-public class BinarySearchTree<E extends Comparable<E>>{ 
-	private class BinarySearchTreeNode<E extends Comparable<E>>{
+public class BinarySearchTree<E extends Comparable<E>> {
+	private class BinarySearchTreeNode<E extends Comparable<E>> {
 		private E elt;
 		private BinarySearchTreeNode<E> left;
 		private BinarySearchTreeNode<E> right;
-	
-		public void add(E elt){
-			if (this.elt.compareTo(elt)>0){
-				if (left==null){
+		private Object node;
+
+		public void add(E elt) {
+			if (this.elt.compareTo(elt) > 0) {
+				if (left == null) {
 					left = new BinarySearchTreeNode<E>(elt);
-				}else{
+				} else {
 					left.add(elt);
 				}
-			}else if(this.elt.compareTo(elt)<0){
-				if(right==null){
+			} else if (this.elt.compareTo(elt) < 0) {
+				if (right == null) {
 					right = new BinarySearchTreeNode<E>(elt);
-				}else{
+				} else {
 					right.add(elt);
 				}
 			}
 		}
-		
-		public boolean contains(E john){
-			int status=elt.compareTo(john);
-			if(status==0){
+
+		public boolean contains(E elt) {
+			int status = this.elt.compareTo(elt);
+			if (status == 0) {
+
 				return true;
-			}else if (status>0){
-				if (left !=null){
-					left.contains(john);
-				}else{
+			} else if (status > 0) {
+				if (left != null) {
+
+					return left.contains(elt);
+				} else {
 					return false;
 				}
-			}else if(status<0){
-				if (right != null){
-					right.contains(john);
-				}else{
+			} else if (status < 0) {
+				if (right != null) {
+
+					return right.contains(elt);
+				} else {
+
 					return false;
 				}
 			}
-			System.out.println("Status " +status);
 			return false;
 		}
-		public void display(){
-			if (left != null){
+
+		public void display() {
+			if (left != null) {
 				left.display();
 			}
-			
+
 			System.out.println(elt.toString());
-			
-			if (right != null){
+
+			if (right != null) {
 				right.display();
 			}
 		}
-		
-		public BinarySearchTreeNode(E elt){
-			this.elt=elt;
-		}
-		
-	public int compare(E elt){
-		return this.elt.compareTo(elt);
-	}
-	
-	}
-	
 
-	private BinarySearchTreeNode<E> root;
-	
-	public BinarySearchTree(){
+		public BinarySearchTreeNode(E elt) {
+			this.elt = elt;
+		}
+
+		public int compare(BinarySearchTreeNode<E> joh) {
+			return this.elt.compareTo(joh.getElt());
+		}
+
+		public BinarySearchTreeNode<E> findNode(BinarySearchTreeNode<E> root, BinarySearchTreeNode<E> previous, E elt) {
+			BinarySearchTreeNode<E> find = root;
+
+			if (find.contains(elt)) {
+
+				if (elt.compareTo(find.getElt()) == 0) {
+					return previous;
+				} else if (elt.compareTo(find.getElt()) > 0) {
+					previous = find;
+					return findNode(find.right, previous, elt);
+				} else {
+					previous = find;
+					return findNode(find.left, previous, elt);
+				}
+			}
+
+			return null;
+		}
+
+		public void delete(BinarySearchTreeNode<E> root, E elt) {
+			BinarySearchTreeNode<E> node = findNode(root, null, elt);
+			// System.out.println(node.getElt());
+			if (node.left != null) {
+				if (node.left.getElt().compareTo(elt) == 0) {
+					BinarySearchTreeNode<E> buff= node.left;
+					node.left=findLeft(node.left);
+					node.left=buff.left;
+					node.right=buff.right;
+				}
+			}
+			if (node.right != null) {
+				if (node.right.getElt().compareTo(elt) == 0) {
+					BinarySearchTreeNode<E> buff= node.right;
+					node.right = findRight(node.right);
+					node.right=buff.right;
+					node.left=buff.left;
+				}
+			}
+
+		}
+		public BinarySearchTreeNode<E> findLeft(BinarySearchTreeNode<E> node){
+			if (node.right != null){
+				node=node.right;
+			}
+			
+			return node;
+		}
+		public BinarySearchTreeNode<E> findRight(BinarySearchTreeNode<E> node){
+			if (node.left != null){
+				node=node.left;
+			}
+			
+			return node;
+		}
+
+		public E getElt() {
+			return this.elt;
+		}
+
 	}
-	
-	public int compare(E elt){
-		return root.compare(elt);
+
+	public BinarySearchTreeNode<E> root;
+
+	public BinarySearchTree() {
 	}
-	
-	public void add(E elt){
-		if (root == null){
-			root=new BinarySearchTreeNode<E>(elt);
-		}else{
+
+	public void delete(E elt) {
+		if (root != null) {
+			root.delete(root, elt);
+		}
+
+	}
+
+	// public int compare(E elt) {
+	// return root.compare(elt);
+	// }
+
+	public void add(E elt) {
+		if (root == null) {
+			root = new BinarySearchTreeNode<E>(elt);
+		} else {
 			root.add(elt);
 		}
 	}
 
-	public boolean contains(E elt){
+	public boolean contains(E elt) {
 		return root.contains(elt);
 	}
 
-	public void display(){
+	public void display() {
 		root.display();
 	}
 
+	public static void main(String[] args) {
 
-	
-	public static void main(String[] args){
-		
 		BinarySearchTree<Integer> tree = new BinarySearchTree<Integer>();
 		Scanner scan = new Scanner(System.in);
-		while (scan.hasNext()){
-			int lol=scan.nextInt();
+		while (scan.hasNext()) {
+			int lol = scan.nextInt();
 			tree.add(lol);
-			System.out.println(tree.compare(lol));
+			// System.out.println(tree.compare(lol));
 		}
+		tree.delete(5);
 		tree.display();
-		if (tree.contains(1)==true){
-			System.out.println("5 is in the tree");
-		}
-		if (tree.contains(101)==false){
-			System.out.println("101 is not in the tree");
-		}
-		
+
 	}
-	
-	
+
 }
